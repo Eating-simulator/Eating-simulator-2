24ac68673d610bdcdf3353a2352b91b62eb4fccb
Landon <landon.fox@outlook.jp>
2021-01-09 16:21:24 -0700

    Added opentab function

--- es.js
+++ es.js
@@ -1,7 +1,7 @@
 var yeet = "yeet"
 var es = {
 	opentab(tab){
-		var content = document.querySelector("."+tab).innerHTML;
+		var content = document.querySelector("#"+tab).innerHTML;
 		document.querySelector(".tab").innerHTML = content;
 	}
 }
