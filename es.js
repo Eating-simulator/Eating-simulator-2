5cbb09787a9d0f3244fcaa3b0229455e33ab7264
Landon <landon.fox@outlook.jp>
2021-01-10 10:43:25 -0700

    Store

--- es.js
+++ es.js
@@ -2,6 +2,10 @@ var yeet = "yeet"
 var es = {
 	opentab(tab){
 		var content = document.querySelector("#"+tab).innerHTML;
-		document.querySelector(".tab").innerHTML = content;
+		document.querySelector("#tab").innerHTML = content;
+	},
+	data:{
+		money:0,
+		calories:300
 	}
 }
